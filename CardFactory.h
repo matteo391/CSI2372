ccc38658296b4b0191042ec2d3424adf86168b23
Fubuki_Waifu <qq2212640070@gmail.com>
2020-12-05 18:16:24 -0500

    Add files via upload

--- CardFactory.h
+++ CardFactory.h
@@ -1,11 +1,31 @@
 #include <iostream>
 #include "Deck.h"
+#include <random>
+#include "Card.h"
+
+
 using namespace std;
 
 class CardFactory {
+
+private:
+	CardFactory();
 	Deck deck;
-public: CardFactory();
-		static CardFactory* getFactory(); //Only one copy of Card Factory
-		Deck getDeck();
+	Blue blue;
+	Chili chili;
+	Stink stink;
+	Green green;
+	soy soys;
+	black blacks;
+	Red red;
+	garden gardens;
+
+
+public: 
+	
+	~CardFactory();
+	static CardFactory* getFactory(); //Only one copy of Card Factory
+	Deck getDeck();
+	
 };
 
