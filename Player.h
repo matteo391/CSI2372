61b183d35d988588d3823fc63650c58ed3444bbb
Fubuki_Waifu <qq2212640070@gmail.com>
2020-12-07 15:00:14 -0500

    Add files via upload

--- Player.h
+++ Player.h
@@ -1,18 +1,24 @@
 #include <iostream>
 #include "Chain.h"
 #include "Hand.h"
+#include <array>
+#include <vector>
 #include "CardFactory.h"
 using namespace std;
 
 class Player {
-private: string name;
-		 int coins = 0;
+private: string namePlayer;
+		 int numcoins = 0;
 		 int numChains = 0;
+		 int Maxnumberchains;
 		 vector<Chain<Card*>> chains;
+		 Chain chain1;
+		 Chain chain2;
+		 Chain chain3;
 		 Hand* hand;
-public: Player(string&);
+public: Player(std::string& name);
 		Player(istream&, const CardFactory*);
-		string getName();
+	    std::string getName();
 		int getNumCoins();
 		Player& operator+=(int);
 		int getMaxNumChains();
